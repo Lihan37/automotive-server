dbe5429b7bfdef2358704aa9f09068c5fa195a36
Eanur Lihan <lihan.lihu@gmail.com>
2023-10-21 14:59:27 +0600

    updated post and get for add to cart

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -31,14 +31,63 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+    
     await client.connect();
+
+    const carsCollection = client.db('carsDB').collection('cars');
+    const cartCollection = client.db('carsDB').collection('cart');
+
+    app.get('/cars/:id', async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = { _id: new ObjectId(id) }
+      const result = await carsCollection.findOne(query)
+      console.log(result);
+      res.send(result);
+    })
+
+
+
+    app.get('/cars', async (req, res) => {
+      const cursor = carsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.post('/cars', async (req, res) => {
+      const newCar = req.body;
+      console.log(newCar);
+      const result = await carsCollection.insertOne(newCar);
+      res.send(result);
+    })
+
+    app.get('/addToCart', async (req, res) => {
+      const cursor = cartCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.post('/addToCart', async (req, res) => {
+      
+        const carDetails = req.body;
+        console.log(carDetails);
+        const result = await cartCollection.insertOne(carDetails);
+        res.send(result);
+      
+      
+    });
+
+    // ...
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
@@ -46,10 +95,10 @@ run().catch(console.dir);
 
 
 
-app.get('/', (req, res)=>{
-    res.send('automotive server is running')
+app.get('/', (req, res) => {
+  res.send('automotive server is running')
 })
 
-app.listen(port, ()=>{
-    console.log(`automotive server is running on port : ${port}`)
+app.listen(port, () => {
+  console.log(`automotive server is running on port : ${port}`)
 })
